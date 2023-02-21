d7fcec9c7ee3e0ebd74b25473f3e2e5c875029c4
najib.hashi@accenture.com <najib.hashi@accenture.com>
2023-02-20 19:25:38 -0500

    Modified main java class

--- src/com/hashi/Main.java
+++ src/com/hashi/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome Jenkins Integration with GitHub.");
+        System.out.println("The Second integration process happened.");
     }
 }
